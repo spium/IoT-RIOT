92f45662bf2473f58bdab7a6403a19a6c94d876a
giovanni <gpiumatti89@gmail.com>
2014-05-19 15:53:56 +0200

    Test to access transceiver interface.

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "board.h"
 #include "vtimer.h"
+#include "net_if.h"
 #include <msp430.h>
 
 #define MSEC ((uint32_t) 1000)
@@ -12,24 +13,28 @@ int main(void) {
 	P1DIR |= BIT0;					// Set P1.0 to output direction
 	P3DIR |= BIT6;
 
-	//set clock output port
-	P3DIR |= BIT0;	//P3.0 set to output
-	P3SEL |= BIT0;	//P3.0 set to peripheral module function
-
-	PMAPKEYID = PMAPPW;		//gain write access to port mapping registers
-	P3MAP0 = PM_MCLK;	//send MCLK to P3.0
-	PMAPKEYID = 0;
-
 	P1OUT &= ~BIT0;	//turn off green led
-	P3OUT |= BIT6;	//turn on red led
+	P3OUT &= ~BIT6;	//turn on red led
 
 
-	while(1) {
-		__delay_cycles(F_CPU);
+	int n = 0;
+	while(n < 3) {
+		__delay_cycles(F_CPU/2);
 //		vtimer_usleep(100 * MSEC);
 		//toggle leds
-		P1OUT ^= BIT0;				// Toggle P1.0 using exclusive-OR
-		P3OUT ^= BIT6;
+		P1OUT ^= BIT0;		// Toggle P1.0 using exclusive-OR
+		++n;
 	}
+
+	P1OUT &= ~BIT0;	//turn off green led
+
+	int iface = net_if_init_interface(NET_IF_L3P_RAW, TRANSCEIVER_CC1100);
+
+	if(iface < 0) P3OUT |= BIT6;
+	else P1OUT |= BIT0;
+
+
+
+
 	return 0;
 }
